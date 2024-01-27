00b8bebfd803b86f602f6ae049bcc7a72b10530b
EmanuilStefan <122681922+EmanuilStefan@users.noreply.github.com>
2024-01-27 14:17:05 +0200

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,18 @@
+﻿int numOfLines = int.Parse(Console.ReadLine());
+
+HashSet<string> lines = new HashSet<string>();
+
+for (int i = 0; i < numOfLines; i++)
+{
+    string[] inputElements = Console.ReadLine()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    for (int j = 0; j < inputElements.Length; j++)
+    {
+        lines.Add(inputElements[j]);
+    }
+}
+
+string[] elements = lines.OrderBy(x => x).ToArray();
+
+Console.WriteLine(string.Join(" ", elements).TrimEnd());
